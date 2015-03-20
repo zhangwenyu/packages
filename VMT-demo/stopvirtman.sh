9b08280387b4b93bd3a4cda0258005fdcb545a2f
unknown <zhangwenyu.mail@163.com>
2015-03-20 09:27:04 +0800

    modified:   VMT-demo/stopvirtman.sh

--- VMT-demo/stopvirtman.sh
+++ VMT-demo/stopvirtman.sh
@@ -4,7 +4,7 @@ FILE_NAME="virtmanserver.py"
 
 PID = ps aux | grep $FILE_NAME | grep "python" | grep -v "$$" | grep -v grep | awk '{print $2}'
 
-echo 'find ' $FILE_NAME ' process id:' $PID
+echo 'find ' $FILE_NAME ' process id: ' $PID
 
 for i in PID
 do
